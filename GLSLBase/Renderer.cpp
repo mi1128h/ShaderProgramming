8f544468fbfdf141c3e532464b91e91ed30ba3f3
mi1128h <mihyeon001128@gmail.com>
2022-04-20 05:43:05 +0900

    삼각형 컬러 per vertex

--- GLSLBase/Renderer.cpp
+++ GLSLBase/Renderer.cpp
@@ -72,12 +72,26 @@ void Renderer::CreateVertexBufferObjects()
 	float lecture2[]
 		=
 	{
-		0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0
+		0.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		1.0, 1.0, 0.0
 	};	// 9 floats array
 
 	glGenBuffers(1, &m_VBOLecture2);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture2);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(lecture2), lecture2, GL_STATIC_DRAW);
+
+	float lecture3[]
+		=
+	{
+		0.0, 0.0, 0.0, 1, 0, 0, 1,
+		1.0, 0.0, 0.0, 0, 1, 0, 1,
+		1.0, 1.0, 0.0, 0, 0, 1, 1
+	};	// 21 floats array
+
+	glGenBuffers(1, &m_VBOLecture3);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture3);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(lecture3), lecture3, GL_STATIC_DRAW);
 }
 
 void Renderer::AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
@@ -339,16 +353,21 @@ void Renderer::Lecture3()
 	GLuint shader = m_Lecture3Shader;
 	glUseProgram(shader);
 
-	int attribPosition = glGetAttribLocation(m_SolidRectShader, "a_Position");
+	int attribPosition = glGetAttribLocation(shader, "a_Position");
 	glEnableVertexAttribArray(attribPosition);
-	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture2);
-	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture3);
+	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, 0);
+	
+	int attribColor = glGetAttribLocation(shader, "a_Color");
+	glEnableVertexAttribArray(attribColor);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture3);
+	glVertexAttribPointer(attribColor, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (GLvoid*)(sizeof(float)*3));
 
 	int uniformLocTime = glGetUniformLocation(shader, "u_Time");
 	glUniform1f(uniformLocTime, gTime);
 
 	int uniformLocColor = glGetUniformLocation(shader, "u_Color");
-	glUniform4f(uniformLocColor, 1, 0, 0, 1);
+	glUniform4f(uniformLocColor, 1, 1, 1, 1);
 
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 
--- GLSLBase/Shaders/lecture3.fs
+++ GLSLBase/Shaders/lecture3.fs
@@ -2,9 +2,11 @@
 
 layout(location=0) out vec4 FragColor;
 
+in vec4 v_Color;
+
 uniform vec4 u_Color = vec4(1, 1, 1, 1);
 
 void main()
 {
-	FragColor = vec4(u_Color);
+	FragColor = vec4(v_Color);
 }
